04976876d48d87853637a2dca5fdf51163863d97
yannick97 <yann.kuhar@gmail.com>
2021-01-16 17:36:10 +0100

    Mapper works on bunny point cloud.

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import sys
-from sklearn.datasets import make_circles
+
 from Mapper import Mapper
+from ReadPlyFile import ReadPlyFile
+from sklearn.datasets import make_circles
 
 
 def filter_x(point):
@@ -9,6 +11,11 @@ def filter_x(point):
 
 def main(args):
     data = make_circles(100, shuffle=True)[0]
+
+    # print(data)
+
+    data = ReadPlyFile('data/bun000.ply', 0.02).get_data()
+
     mapper = Mapper(data, resolution=0.2, overlap=0.4, cluster_alg='kmeans', max_clusters=5, filter=filter_x)
     graph = mapper.run()
     print(graph)
---
+++ ReadPlyFile.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from plyfile import PlyData
+
+
+class ReadPlyFile:
+    def __init__(self, path, percentage):
+        self.path = path
+        self.percentage = percentage
+
+    def sample_data(self, X):
+            idxs = np.random.choice(len(X), int(len(X) * self.percentage))
+            sample = X[idxs]
+
+            return sample
+
+    def get_data(self):
+        f = open(self.path, 'rb')
+        data = PlyData.read(f)
+        S = []
+
+        for i in range(len(data['vertex']['x'])):
+            S.append((data['vertex']['x'][i], data['vertex']['y'][i], data['vertex']['z'][i]))
+
+        return self.sample_data(np.array(S))
